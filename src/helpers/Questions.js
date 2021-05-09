f48f9b82610bdfd4ee64a18bdeb752ff72f97586
dipeshdelhi98 <63038894+dipeshdelhi98@users.noreply.github.com>
2021-05-09 10:31:11 -0700

    first

---
+++ src/helpers/Questions.js
@@ -0,0 +1,34 @@
+export const Questions = [
+  {
+    prompt: "What is the capital of India?",
+    optionA: "Maharastra",
+    optionB: "Haryana",
+    optionC: "Chennai",
+    optionD: "Delhi",
+    asnwer: "optionD",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "HTML",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+  },
+  {
+    prompt: "Which of following is not a social media platform ?",
+    optionA: "Amazon",
+    optionB: "Instagram",
+    optionC: "Twitter",
+    optionD: "Facebook",
+    asnwer: "optionA",
+  },
+];
